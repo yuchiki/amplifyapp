c6b0dbe13dc04ac56cd6e1096cbb2a0b445ef8d4
yuchiki <14884013+yuchiki@users.noreply.github.com>
2023-12-26 16:33:12 +0900

    no -> niaru

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          この上のくるくる回っているロゴが手書きできなくていつも困っている
+          この上にあるくるくる回っているロゴが手書きできなくていつも困っている
         </p>
       </header>
     </div>
